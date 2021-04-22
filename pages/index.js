01cbda5e4b001fea625d37b85a824d8301297484
pumpkin <reeshul@hotmail.com>
2021-04-22 17:42:04 +0100

    Added empty arrays to fetch functions

--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,64 @@ import Map from "../components/Map";
 import Location from "../components/Location";
 import { apiKey } from "../app.config";
 
+let teamIdArray = []
+let upcomingFixturesHomeTeamIdArray = []
+
 export default function Home() {
-	function fetchData() {
+	async function fetchData() {
+		console.log("3")
 		let date = returnDate()
 		fetch(`https://soccer.sportmonks.com/api/v2.0/fixtures/date/${date}?api_token=${apiKey}`)
 			.then(response => response.json())
-			.then(data => console.log(data));
-			
+			.then(data => upcomingFixturesHomeTeamIdArray.push(data));
 	}
-function returnDate() {
-	var today = new Date();
-	var dd = String(today.getDate()).padStart(2, '0');
-	var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-	var yyyy = today.getFullYear();
 
-	return yyyy + '-' + mm + '-' + dd;
-}		
+	function fetchTeams() {
+		console.log("4")
+		fetch("/api/teams").then(res => res.json()).then(data => teamIdArray.push(data))
+	}
+
+	function returnDate() {
+		var today = new Date();
+		var dd = String(today.getDate()).padStart(2, '0');
+		var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+		var yyyy = today.getFullYear();
+
+		return yyyy + '-' + mm + '-' + dd;
+	}
 
 	const [count, setCount] = useState(0);
 	const [teams, setTeams] = useState([]);
+
+
 	useEffect(() => {
-		fetchData();
-	});
+		console.log("1")
+		loadFixtures();
+
+
+	})
+
+
+	async function loadFixtures() {
+		console.log("2")
+		await fetchData();
+		await fetchTeams();
+		console.log("5")
+		
+		console.log(upcomingFixturesHomeTeamIdArray.length);
+	}
+
+	// function renderFixtures() {
+	// 	if (upcomingFixturesHomeTeamIdArray.length === 0)
+	// }
+
+
+
+
+
+
+
+	// console.log(upcomingFixturesHomeTeamIdArray)
 	return (
 		<div>
 			<Head>
